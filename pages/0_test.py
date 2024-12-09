58a6c1cf4a626f63763b3e231f1a08a044e9fc88
Rebecca-Leee <zhengli97rebecca@163.com>
2024-12-09 18:15:32 +0000

    database

---
+++ pages/0_test.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+import pymysql
+from sqlalchemy import create_engine, text
+
+def get_sql_engine():
+    # 数据库
+    mysql_config = {
+        "db": "effective_baking",
+        "host": "mysql.sqlpub.com",
+        "user": "rebecca_leee",
+        "password": "8p8cMQhcstwVg4q5",
+        "port": 3306,
+    }
+    engine = create_engine(
+        "mysql+pymysql://{}:{}@{}:{}/{}".format(mysql_config['user'], mysql_config['password'], mysql_config['host'],
+                                                mysql_config['port'], mysql_config['db']))
+    return engine
+
+# 连接到MySQL数据库
+conn = pymysql.connect(host='mysql.sqlpub.com', user='rebecca_leee', password='8p8cMQhcstwVg4q5', db='effective_baking')
+ 
+# 创建一个Cursor:
+cursor = conn.cursor()
+ 
+# 执行一条SQL查询语句:
+cursor.execute("SELECT VERSION()")
+ 
+# 获取查询结果:
+data = cursor.fetchone()
+print("Database version : %s " % data)
+ 
+# 关闭Cursor和Connection:
+cursor.close()
+conn.close()
+
+engine = get_sql_engine()
+
+
+data = {"a":[1,2],"b":["x","y"]}
+df = pd.DataFrame(data)
+
+df_edit = st.data_editor(df)
+
+df_edit.to_sql('test_df', con=engine, if_exists='replace', index=False)
+
+test = pd.read_sql_query(text('select * from test_df'), con=engine.connect())
+st.write(test.head())
