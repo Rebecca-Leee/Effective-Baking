5f653028d71eb82ad02e29f3395f636112bdbafa
Rebecca-Leee <zhengli97rebecca@163.com>
2024-12-09 18:01:05 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Effective Baking Test")
 st.write("Hello User!")
+
+
+data = {
+    'Column1': [1, 2, 3, 4],
+    'Column2': ['A', 'B', 'C', 'D']
+}
+ 
+df = pd.DataFrame(data)
+ 
+st.write(data)
+
+df.to_csv("data.csv")
+         
+data = pd.read_csv("data.csv")
